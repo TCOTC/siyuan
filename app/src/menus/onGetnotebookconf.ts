0a39ae97fe6e19468cb5af5563059ae46e23f7d5
Jeffrey Chen <78434827+TCOTC@users.noreply.github.com>
2026-05-24 21:46:53 +0800

    overflow: hidden 导致对话框外的关闭按钮不显示、难以拖拽调整边界

--- app/src/menus/onGetnotebookconf.ts
+++ app/src/menus/onGetnotebookconf.ts
@@ -50,7 +50,7 @@ export const onGetnotebookconf = (data: INotebookConf) => {
     const titleHTML = `<div class="fn__flex">${escapeHtml(data.name)}
 <div class="fn__space"></div>
 <button class="b3-button b3-button--small fn__flex-center">${window.siyuan.languages.copy} ID</button></div>`;
-    const contentHTML = `<div class="b3-dialog__content" style="background-color: var(--b3-theme-background);">
+    const contentHTML = `<div class="b3-dialog__content">
 <div class="b3-label config-item config-item--save-path">
     <div class="config-name">${window.siyuan.languages.fileTree12}</div>
     <div class="b3-label__text">${window.siyuan.languages.fileTree13}</div>
